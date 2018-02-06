4a7e2d4aca21f799b67a229cd8da13e11bff6a98
David Otiego <davidotiego@yahoo.co.uk>
2018-02-06 12:21:11 -0600

    Schedule
    Model blocks object removed. Goes to that from entry.
    Factory methods from service

--- src/main/java/com/mumscheduler/schedule/factory/ScheduleFactory.java
+++ src/main/java/com/mumscheduler/schedule/factory/ScheduleFactory.java
@@ -5,6 +5,9 @@ import com.mumscheduler.schedule.model.ScheduleFacade;
 
 public final class ScheduleFactory {
 
+	public static final String INSUFFICIENT_BLOCKS = "You don't have a sufficient number of blocks to generate this schedule.";
+	public static final String NO_SCHEDULE_FOUND = "No schedule found !";
+
 	public static ScheduleFacade createEmptyScheduleFacade() {
 		return new ScheduleFacade(true, "", null);
 	}
@@ -12,5 +15,16 @@ public final class ScheduleFactory {
 	public static ScheduleFacade createScheduleFacade(Schedule schedule) {
 		return new ScheduleFacade(true, "", schedule);
 	}
-	
+
+	public static ScheduleFacade validateSchedule(Schedule schedule) {
+		ScheduleFacade sf = ScheduleFactory.createScheduleFacade(schedule);
+
+		if (schedule == null) {
+			sf.setActionSuccess(false);
+			sf.setActionResponse(NO_SCHEDULE_FOUND);
+		}
+
+		return sf;
+	}
+
 }
